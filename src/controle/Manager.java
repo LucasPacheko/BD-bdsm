0581ccae0547f55e4c723d4253acb174d1e813c8
lucas <lucas@PC-Lab>
2019-01-18 17:58:41 -0300

    18/1/2019

--- src/controle/Manager.java
+++ src/controle/Manager.java
@@ -8,25 +8,32 @@ import java.util.HashMap;
 import models.Bloco;
 
 public class Manager {
-	static BufferedReader in;
-	static HashMap<Integer, String> metaDadosAtributos;
+	
 
-	static ArrayList<Bloco> arquivoDeDados = new ArrayList<Bloco>();
-	static int controle = 0;
-	static Bloco currentBlock = null;
+	static HashMap<Integer,Bloco> arquivoDeDados;
+
+	
 	static int tamanhoBlocos = 32768;
-	static int status = 0;
-
+	
 	public static void main(String[] args) throws IOException {
-		
-		in = new BufferedReader(new FileReader("dados"));
-
-		currentBlock = new Bloco(tamanhoBlocos, in.readLine());
-
+		arquivoDeDados= new HashMap<Integer,Bloco>();
+		BufferedReader in = new BufferedReader(new FileReader("dados"));
+		//Primeira linha do arquivo é o header
+		Bloco controle = new Bloco(tamanhoBlocos, in.readLine());
+		arquivoDeDados.put(0, controle);
 		while (in.ready()) {
-
+			
 		}
 
+	}
+	static void addTupla(String tupla){
+		String[] data = tupla.split("\\|");
+		Bloco controle = arquivoDeDados.get(0);
+		Bloco bloco = arquivoDeDados.get(controle.getProximoBloco());
+		if(bloco)
+		if(bloco.putData(data)){
+			
+		}
 	}
 
 	
--- src/models/Bloco.java
+++ src/models/Bloco.java
@@ -21,10 +21,14 @@ public class Bloco {
 			conteudo[i] = bytes[j--];
 			p++;
 		}
-		p+=7;
-		char[] arrayChar =  header.toCharArray();
+		p += 5;
+		char[] arrayChar = header.toCharArray();
+		bytes = ByteBuffer.allocate(2).putShort((short) arrayChar.length).array();
+		for (byte b : bytes) {
+			conteudo[p++] = b;
+		}
 		for (int i = 0; i < arrayChar.length; i++) {
-			conteudo[p+i]=(byte)arrayChar[i];
+			conteudo[p + i] = (byte) arrayChar[i];
 		}
 	}
 
@@ -33,26 +37,68 @@ public class Bloco {
 		System.out.println(bloc.getTamanho());
 		System.out.println(bloc.getHeader());
 	}
-
-	int getTamanho() {
+	public void setProxBlocoLivre(Bloco proxBloco){
+		byte[] b = proxBloco.getIdBlocoArray();
+		for (int i = 1; i < b.length; i++) {
+			conteudo[4+i]=(byte)b[i];
+		}
+	}
+	public int getIdBlocoInt(){
+		byte[] b = {(byte)0,conteudo[1],conteudo[2],conteudo[3]};
+		return ByteBuffer.wrap(b).getInt();
+	}
+	byte[] getIdBlocoArray(){
+		byte[] b = {(byte)0,conteudo[1],conteudo[2],conteudo[3]};
+		return b;
+	}
+	public int getTamanho() {
 		byte[] b = { (byte) 0, conteudo[1], conteudo[2], conteudo[3] };
 		final ByteBuffer bb = ByteBuffer.wrap(b);
 
 		return bb.getInt();
 	}
-
-	int getProximoBloco() {
+	public int getProximoBloco() {
 		byte[] b = { conteudo[5], conteudo[6], conteudo[7], conteudo[8] };
 		return ByteBuffer.wrap(b).getInt();
 	}
-
-	String getHeader() {
+	public int getSizeheader() {
 		byte[] b = { (byte) 0, (byte) 0, conteudo[9], conteudo[10] };
 		int size = ByteBuffer.wrap(b).getInt();
-		char[] arrayChar =  new char[size];
-		for (int i=0 ; i < 11+size; i++) {
-			arrayChar[i]=(char)conteudo[11+i];
+		return size;
+	}
+	public String getHeader() {
+		int size = getSizeheader();
+		char[] arrayChar = new char[size];
+		for (int i = 0; i < size; i++) {
+			arrayChar[i] = (char) conteudo[11 + i];
 		}
-		return String.copyValueOf(arrayChar);
+		String a = new String(arrayChar);
+		return a;
+	}
+	public boolean isFull(){
+		if(conteudo[4]!=0){
+			return true;
+		}
+		return false;
+	}
+	public boolean putData(String[] data) {
+		// TODO Auto-generated method stub
+		int sizeTupla=0;
+		int written= getBytesUsados();
+		int sizeBloco = getTamanho();
+		for (int i = 0; i < data.length; i++) {
+			sizeTupla=data[i].length();
+		}
+		if(sizeBloco+sizeTupla>sizeBloco)return false;
+		written++;
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[i].length(); j++) {
+				conteudo[written++]=(byte)data[i].charAt(j);
+			}
+		}
+	}
+	int getBytesUsados(){
+		byte[] b = {(byte)0,conteudo[5],conteudo[6],conteudo[7]};
+		return ByteBuffer.wrap(b).getInt();
 	}
 }
